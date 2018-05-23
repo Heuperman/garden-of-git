af5a0d6c829ab6edac58ee50e9bd3638264b8d55
kees <cheuperman@gmail.com>
2018-05-23 07:26:09 +0100

    No longer allow for duplicate guesses and only reduce count on a wrong guess. Update README file

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 A repository containing several small projects and tests.
 
+01 - Hangman: play a game of hangman against the computer.
+
 To use type `git clone https://github.com/heuperman/garden-of-git.git`
 Then run projects using Ruby and follow the instructions on the terminal
 
--- hangman.rb
+++ hangman.rb
@@ -4,63 +4,67 @@ words = ["hardware","keyboard","memory","disk","computer","display","software","
 @word = words.sample
 @guesses = []
 @word_display = "_"*@word.length
-@guesses_remaining = 10
+@guesses_remaining = 11
 
 puts "Let's play a game of hangman"
 puts "I'll pick the word"
 puts "Word: #{@word_display}"
 
 def guess
-    puts "Please guess a letter"
-    user_guess = gets.chomp.downcase
+  puts "Please guess a letter"
+  user_guess = gets.chomp.downcase
 
-# Check if user input is a letter and only 1 letter
-    if user_guess.match?(/[a-z]/) && user_guess.size == 1
-        guess_checker(user_guess)
-    else
-        puts "Invalid input. Please enter one letter only"
-        guess
-    end
+# Check if user input has not been guessed before, is a letter is and only 1 letter
+  if @guesses.include?(user_guess)
+    puts "You have already guessed '#{user_guess}' before"
+    guess
+  elsif user_guess.match?(/[a-z]/) && user_guess.size == 1
+    guess_checker(user_guess)
+  else
+    puts "Invalid input. Please enter one letter only"
+    guess
+  end
 end
 
 def guess_checker(user_guess)
-    if @word.include?(user_guess)
-        puts "You got lucky, '#{user_guess}' is in my word"
-        word_checker(user_guess)
-        puts "Word: #{@word_display}"
-    else
-        puts "Sorry, '#{user_guess}' is not in my word"
-        puts "Word: #{@word_display}"
-    end
+  if @word.include?(user_guess)
+    puts "You got lucky, '#{user_guess}' is in my word"
+    word_checker(user_guess)
+  else
+    puts "Sorry, '#{user_guess}' is not in my word"
+    @guesses_remaining -= 1
+  end
 
+  puts "Word: #{@word_display}"
+
+  if @word_display == @word
+    puts "Congratulations, you guessed my word!"
+  elsif @guesses_remaining == 0
+    puts "Game over, I win!"
+  else
     @guesses << user_guess
     puts "You've guessed: #{@guesses.join(' ')}"
-    @guesses_remaining -= 1
     puts "Guesses remaining: #{@guesses_remaining}"
-
-    if @word_display == @word
-        puts "Congratulations, you guessed my word!"
-    elsif @guesses_remaining == 0
-        puts "Game over, I win!"
-    else
-        guess
-    end
+    guess
+  end
 end
 
+# checks for multiple instances of the guessed letter in the word
 def word_checker(user_guess)
-    i = -1
-    all = []
-    while i = @word.index(user_guess,i+1)
-        all << i
-    end
+  i = -1
+  instances = []
+# Checks for the index of the first instance of user_guess AFTER index number i
+# Stops when @word.index(user_guess,i+1) returns nil
+  while i = @word.index(user_guess,i+1)
+      instances << i
+  end
 
-    all.each do |x| @word_display[x] = user_guess end
-
+# replace dashes in word display with the correctly guessed letters
+    instances.each do |i| @word_display[i] = user_guess end
 end
+
 guess
 
 # to do:
-# don't allow of multiple entries of same character
-# make it so the final  message does not include guesses or guesses_remaining
 # add visual of gallows
 # let player pick the word
